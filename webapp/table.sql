82531886c5ed1c0c6c636348670eaa901e220cd1
Rishabh Bhandari <41220684+RishabhKodes@users.noreply.github.com>
2019-07-10 20:18:21 +0530

    Create table.sql

---
+++ webapp/table.sql
@@ -0,0 +1,13 @@
+use test;
+ 
+CREATE TABLE employee
+(
+id int(5) primary key,
+firstname varchar(30),
+lastname varchar(30),
+emp_id int(5)
+); 
+
+INSERT into employee values(1, "virat", "kohli", 1);
+INSERT into employee values(2, "shikhar", "dhawan", 2);
+INSERT into employee values(3, "bhuvi", "sharma", 3);
